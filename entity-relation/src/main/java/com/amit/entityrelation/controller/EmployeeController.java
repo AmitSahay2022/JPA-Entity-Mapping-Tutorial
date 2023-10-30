d7743932c526bbc8d03c7d204127b2249a1c2636
Sahay Software <sahay.mtech2013@gmail.com>
2023-10-30 15:32:28 +0530

    Bi directional mapping created but still owner is Employee

--- entity-relation/src/main/java/com/amit/entityrelation/controller/EmployeeController.java
+++ entity-relation/src/main/java/com/amit/entityrelation/controller/EmployeeController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class EmployeeController {
 	@GetMapping
 	public ResponseEntity<List<Employee>> getAllEmpoyee() {
 		return new ResponseEntity<List<Employee>>(repository.findAll(), HttpStatus.OK);
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+		repository.deleteById(id);
+		return new ResponseEntity<String>("Employee Deleted", HttpStatus.OK);
 	}
 }
--- entity-relation/src/main/java/com/amit/entityrelation/entity/Address.java
+++ entity-relation/src/main/java/com/amit/entityrelation/entity/Address.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
@@ -16,4 +17,10 @@ public class Address {
 	private String city;
 	private String state;
 	private int pinCode;
-}
+	@OneToOne(mappedBy = "address") //This statement is saying employee class has field address 	   
+	private Employee employee;            //which will take care Foreign key for mapping
+}              //In other words Employee is the owner in this relationship
+/*
+ * mappedBy -->when we use mappedBy property then no extra column is created
+ * 
+ */
